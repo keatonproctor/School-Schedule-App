29f75f064ed9bcd8aa9401258c6f99c449d21d11
Keaton Proctor <keatonproctor@Keatons-MacBook-Pro.local>
2020-05-22 15:24:02 -0600

    app setup

---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='home'>
+        <h1>Course Scheduler</h1>
+      </div>
+    );
+  }
+}
