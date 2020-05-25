fb921dbc7946251b3004aefb7a2763d33a450b16
Keaton Proctor <keatonproctor@Keatons-MacBook-Pro.local>
2020-05-25 10:11:38 -0600

    overflow scrolling on library component

--- src/components/library/library.js
+++ src/components/library/library.js
@@ -1,19 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import LibraryCourse from './libraryCourse';
+import * as actions from "../../actions";
 
+import LibraryCourse from "./libraryCourse";
 
 class Library extends Component {
-    render() {
-        return (
-            <div className="library">
-                <h1 className="library__title">Course Library</h1>
-                <LibraryCourse/>
-                <LibraryCourse/>
-                <LibraryCourse/>
-            </div>
-        )
-    }
+  componentWillMount() {
+    this.props.fetchCourses();
+  }
+
+  renderCourses() {
+    const data = this.props.courses;
+
+    return data.map((course, index) => {
+      return <LibraryCourse {...course} key={index} />;
+    });
+  }
+
+  render() {
+    return (
+      <div className="library">
+        <div className="library__container">
+          <h1 className="library__container__title">Course Library</h1>
+          {this.renderCourses()}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Library;
+function mapStateToProps(state) {
+  return {
+    courses: state.courses
+  };
+}
+
+export default connect(mapStateToProps, actions)(Library);
