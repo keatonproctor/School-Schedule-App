9c1baee042417c872911c303dc206e4ecce55c51
Keaton Proctor <keatonproctor@Keatons-MacBook-Pro.local>
2020-05-25 10:08:04 -0600

    installed and connected redux devtools to application

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,24 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter } from "react-router-dom";
-import Home from "./components/home";
-import reducers from "./reducers";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import Home from './components/home';
+import reducers from './reducers';
 
-import "./style/main.scss";
+const createStoreWithMiddleware = applyMiddleware()(compose(window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore));
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './style/main.scss';
 
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-      <BrowserRouter>
-        <Home />
-      </BrowserRouter>
-    </Provider>,
-    document.querySelector(".app-wrapper")
-  );
+      <Home />
+    </Provider>
+    , document.querySelector('.app-wrapper'));
 }
 
-document.addEventListener("DOMContentLoaded", main);
+document.addEventListener('DOMContentLoaded', main);
