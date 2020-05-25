81d1cf81cf4fedceb6a3edab62ee6f97e15273e4
Keaton Proctor <keatonproctor@Keatons-MacBook-Pro.local>
2020-05-25 10:12:33 -0600

    rendering checkmark and add remove button based on enrollment status

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,30 +1,66 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../../actions";
 
 import Icon from "../icon";
 import Arrow from "../arrow";
+import Action from "../action";
+
+import AnimateHeight from "react-animate-height";
 
 class LibraryCourse extends Component {
-    render() {
-        return (
-          <div className="library-course">
-            <div className="library-course__title-arrow">
-              <label className="library-course__title">Problem Solving</label>
-              {Icon("fas fa-check", "library-course__icon")}
-            </div>
-            <Arrow className="library-course__arrow" />
-            {/* action button component */}
-            <div className="library-course__description">
-              <label>Course Description</label>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-                fringilla facilisis mi, non pellentesque metus consectetur vel.
-                Quisque dictum, lectus vitae dignissim tincidunt, sapien nibh
-                placerat diam, quis blandit enim nulla in felis.
-              </p>
-            </div>
-          </div>
-        );
-      }
-    }
+  constructor(props) {
+    super(props);
 
-export default LibraryCourse;
+    this.state = {
+      status: true,
+      height: 0
+    };
+  }
+
+  handleCallback = function(status) {
+    let height = this.state.height == 0 ? 'auto' : 0;
+    if (!status) {
+      document.getElementById(this.id).classList.add("library-course-selected");
+    } else {
+      document.getElementById(this.id).classList.remove("library-course-selected");
+    }
+    this.setState({ 
+      status,
+      height
+    });
+  }.bind(this);
+
+  render() {
+    this.id = `library-course-${this.props.id}`;
+    return (
+      <div id={this.id} className="library-course">
+        <div className="library-course__title-check">
+          <div className="library-course__title">{this.props.title}</div>
+          { this.props.enrolled ? Icon("fas fa-check", "library-course__icon") : ''}
+        </div>
+        <Arrow
+          callback={status => this.handleCallback(status)}
+          id={this.props.id}
+          className="library-course__arrow"
+        />
+        <Action
+          id={this.props.id}
+          onClick={() => this.props.toggleEnrolled(this.props.id)}
+          className={`library-course__action ${this.props.enrolled ? 'action-remove' : ''}`}
+        />
+        <AnimateHeight
+          duration={300}
+          height={this.state.height}
+        >
+          <div className="library-course__description">
+            <label>Course Description</label>
+            <p>{this.props.description}</p>
+          </div>
+        </AnimateHeight>
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(LibraryCourse);
